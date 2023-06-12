ff2d004ad8669dedd6365030f99d268947979075
barts <bartsteensma@outlook.com>
2023-06-12 22:15:32 +0200

    refactor: added docstrings

--- lua/khalorg.lua
+++ lua/khalorg.lua
@@ -1,9 +1,17 @@
 local Files = require('orgmode.parser.files')
 
---TODO: docstrings
 --TODO: bugfix: if no empty line is present after the last agenda item, the fold is not recognized
---TDDO: refactor
 --TODO: test with minimal config
+
+-- Module table.
+-- @field calendar string
+-- @field new function
+-- @field edit function
+-- @field delete function
+-- @field edit_all function
+-- @field get_fold_under_cursor function
+-- @field make_exporter function
+-- @field setup function
 local M = {}
 
 -- Get the range of the fold under the cursor of a given type.
@@ -32,19 +40,27 @@ local function export_error(err)
   vim.api.nvim_echo({ { table.concat(err, '\n'), 'ErrorMsg' } }, true, {})
 end
 
+-- Get the text of the fold under the cursor. If no fold is found, return an empty string.
+-- @return string
 M.get_fold_under_cursor = function()
   local status, result = pcall(function() return get_fold_range_at_cursor('section') end)
   if status then
     local lines = vim.api.nvim_buf_get_lines(0, result[1], result[2], true)
     return table.concat(lines, '\n')
+  else
+    return ''
   end
 end
 
+-- Create an exporter function for a given khalorg command.
+-- @param khalorg_command string
+-- @return function
 M.make_exporter = function(khalorg_command)
   return function(exporter)
     local org_agenda_item = M.get_fold_under_cursor()
-    if org_agenda_item == nil then
+    if org_agenda_item == '' then
       export_error({ 'No agenda item found under the cursor' })
+      return
     else
       local cmd = "echo \"" .. org_agenda_item .. "\" | " .. khalorg_command
       return exporter(cmd, '', export_success, export_error)
@@ -52,6 +68,8 @@ M.make_exporter = function(khalorg_command)
   end
 end
 
+-- Setup khalorg.
+-- @param opts table {calendar: string}
 M.setup = function(opts)
   M.calendar = opts.calendar
   M.new = M.make_exporter('khalorg new' .. M.calendar)
--- tests/config.lua
+++ tests/config.lua
@@ -1,23 +1,22 @@
+--This file servers a a minimal config file for testing.
+package.loaded['orgmode'] = nil
+package.loaded['lua.khalorg'] = nil
+
 local orgmode = require('orgmode')
 local khalorg = require('lua.khalorg')
 
-local custom_exports = {
-    e = {
-        label = 'Echo',
-        action = khalorg.make_exporter('echo')
-    },
-}
-
+khalorg.setup({ calendar = '' })
 orgmode.setup_ts_grammar()
 
+--An echo export is added to test if the make_exporter function works properly.
 orgmode.setup({
-  org_custom_exports = custom_exports
+  org_custom_exports = {
+    e = {
+      label = 'Echo',
+      action = khalorg.make_exporter('echo')
+    }
+  }
 })
 
-khalorg.setup({calendar=''})
-
-local function print_fold()
-  print(khalorg.get_fold_under_cursor())
-end
-
-vim.keymap.set('n', '<leader>pf', print_fold)
+-- The binding below is used to test the get_fold_under_cursor function.
+vim.keymap.set('n', '<leader>pf', function() print(khalorg.get_fold_under_cursor()) end)
