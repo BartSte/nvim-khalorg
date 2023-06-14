63157393f7230e7e8b3fe913ef5948340bcf1793
barts <bartsteensma@outlook.com>
2023-06-14 11:16:58 +0200

    bugfix: replaced echo with cat in the test config.lua

--- tests/config.lua
+++ tests/config.lua
@@ -21,12 +21,9 @@ orgmode.setup_ts_grammar()
 --An echo export is added to test if the make_exporter function works properly.
 orgmode.setup({
   org_custom_exports = {
-    e = {
-      label = 'Echo',
-      action = khalorg.make_exporter('echo')
+    c = {
+      label = 'Let the agenda item pass through.',
+      action = khalorg.make_exporter('cat')
     }
   }
 })
-
--- The binding below is used to test the get_fold_under_cursor function.
-vim.keymap.set('n', '<leader>pf', function() print(khalorg.get_fold_under_cursor()) end)
