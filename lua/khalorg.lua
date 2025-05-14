6d142e7eca21ba4ccc836ac25ffe6b59543289e9
0fflineuser <0fflineuser@cock.li>
2025-05-14 20:12:16 +0200

    Replaced iter_matches by iter_captures due to changes in nvim 0.11. Added body of agenda item when adding a new khalorg  entry.

--- lua/khalorg.lua
+++ lua/khalorg.lua
@@ -38,12 +38,12 @@ local function export_error(err)
 end
 
 -- Get the headline, timestamps and properties of the fold under the cursor. If no fold, headline or timestamp is found, return an empty string.
----@return string
-M.get_headline_and_timestamps_in_fold_under_cursor = function()
+-- @return string
+M.get_item_in_fold_under_cursor = function()
   local bufnr = vim.api.nvim_get_current_buf()
   local fold_node = get_fold_node("section")
 
-  -- Get fold limits to remove nested folds in `query:iter_matches` bellow
+  -- Get fold limits to remove nested folds in `query:iter_captures` bellow
   local query_fold_limits = ts_utils.get_query([[
   ((section) @section (#offset! @section))
   ]])
@@ -51,7 +51,7 @@ M.get_headline_and_timestamps_in_fold_under_cursor = function()
   -- local startfold, endfold = vim.treesitter.get_node_range(node)
   local startfold, endfold
   local i = 1
-  for _, captures, metadata in query_fold_limits:iter_matches(fold_node, bufnr) do
+  for _, _, metadata in query_fold_limits:iter_captures(fold_node, bufnr) do
     local range = metadata[1].range
 
     if i == 1 then
@@ -77,27 +77,27 @@ M.get_headline_and_timestamps_in_fold_under_cursor = function()
   ]])
 
   -- This is the text containing the timestamps and the headline
-  local properties, timestamps, headline = nil
-  for _, captures, metadata in query:iter_matches(fold_node, bufnr, startfold, endfold) do
-    if headline == nil then
-      headline = vim.treesitter.get_node_text(captures[2], bufnr)
-    end
+  local properties, timestamps, headline, body = nil, nil, nil, nil
+  for id, node, metadata, _ in query:iter_captures(fold_node, bufnr, startfold, endfold) do
+    local name = query.captures[id]
 
-    if timestamps ~= nil then
-      -- Concatenate the timestamps with '--' when there are multiple
-      timestamps = timestamps .. "--" .. vim.treesitter.get_node_text(captures[5], bufnr)
-    else
-      timestamps = vim.treesitter.get_node_text(captures[5], bufnr)
-    end
-
-    -- The @properties capture is optional
-    if properties == nil and captures[6] then
-      properties = vim.treesitter.get_node_text(captures[6], bufnr)
+    if name == "headline" and headline == nil then
+      headline = vim.treesitter.get_node_text(node, bufnr)
+    elseif name == "timestamp" then
+      if timestamps ~= nil then
+        timestamps = timestamps .. "--" .. vim.treesitter.get_node_text(node, bufnr)
+      else
+        timestamps = vim.treesitter.get_node_text(node, bufnr)
+      end
+    elseif name == "properties" and properties == nil then
+      properties = vim.treesitter.get_node_text(node, bufnr)
+    elseif name == "body" and body == nil then
+      body = vim.treesitter.get_node_text(node, bufnr)
     end
   end
 
   if timestamps and headline then
-    return table.concat({ headline, timestamps, properties or "" }, "\n")
+    return table.concat({ headline, timestamps, properties or "", body or "" }, "\n")
   else
     return ""
   end
@@ -108,7 +108,7 @@ end
 ---@return function
 M.make_exporter = function(khalorg_command)
   return function(exporter)
-    local org_agenda_item = M.get_headline_and_timestamps_in_fold_under_cursor()
+    local org_agenda_item = M.get_item_in_fold_under_cursor()
     if org_agenda_item == "" then
       export_error({ "No agenda item found under the cursor" })
       return
